173745e2f7b90c76271ccbaf4c1119093088f56e
Mateusz Rzepa <mrzepa89@gmail.com>
2020-10-31 21:27:36 +0100

    bench

--- bench/benchmark_2.js
+++ bench/benchmark_2.js
@@ -6,6 +6,8 @@ const addon = require('./../src/addon');
 const itemsjs = require('./../src/index')();
 const _ = require('lodash');
 const Promise = require('bluebird');
+const Facets = require('./../src/facets');
+const facets = new Facets();
 
 var config = {
   aggregations: {
@@ -35,12 +37,26 @@ var input = {
   }
 };
 
+var filters_array = _.map(input.filters, function(filter, key) {
+  return {
+    key: key,
+    values: filter,
+    conjunction: config.aggregations[key].conjunction !== false
+  }
+})
+
+filters_array.sort(function(a, b) {
+  return a.conjunction > b.conjunction ? 1 : -1;
+})
+
+var facets_fields = Object.keys(config.aggregations);
+
 (async function() {
 
-  for (var i = 0 ; i < 0 ; ++i) {
+  for (var i = 0 ; i < 100 ; ++i) {
 
     var index = await itemsjs.index('db_' + i, {
-      json_path: './imdb.json',
+      json_path: './imdb2.json',
       append: false,
       configuration: config
     });
@@ -50,19 +66,33 @@ var input = {
 
   // 1000 req in 700 ms
   // 1428 req / s
-  await Promise.all(_.range(0, 1000, 1))
+  await Promise.all(_.range(0, 100, 1))
   .map(async i => {
 
     var number = i % 10;
-    await itemsjs.search('db_' + number, input, {
+
+    /*await addon.search_facets_async({
+      input: input,
+      filters_array: filters_array,
+      aggregations: config.aggregations,
+      facets_fields, facets_fields,
+      query_ids: null,
+      index_path: './data/db_' + number + '.mdb'
+    })*/
+
+    /*await facets.search_native('./data/db_' + number + '.mdb', input, {
       is_async: true
+    })*/
+
+    await itemsjs.search('db_' + number, input, {
+      //is_async: true
     })
     .then(res => {
       //console.log('res')
       //console.log(res)
     })
     .catch(res => {
-      console.log('catch')
+      //console.log('catch')
       console.log(res)
     });
   }, {
@@ -71,18 +101,21 @@ var input = {
 
   console.log(`bench: ${new Date().getTime() - time}`);
 
-  /*var time = new Date().getTime();
-  var number = i % 10;
+  var time = new Date().getTime();
 
-  addon.search_facets({
-    input: input,
-    filters_array: filters_array,
-    aggregations: config.aggregations,
-    facets_fields, facets_fields,
-    query_ids: null,
-    index_path: './data/db_' + number + '.mdb'
-  })
+  /*for (var i = 0 ; i < 1000 ; ++i) {
 
-  console.log(`bench: ${new Date().getTime() - time}`);*/
+    var number = i % 10;
+    addon.search_facets({
+      input: input,
+      filters_array: filters_array,
+      aggregations: config.aggregations,
+      facets_fields, facets_fields,
+      query_ids: null,
+      index_path: './data/db_' + number + '.mdb'
+    })
+  }*/
+
+  console.log(`bench: ${new Date().getTime() - time}`);
 
 })();
